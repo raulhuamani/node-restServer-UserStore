958630411fe7e0230a1175a11fcc970915ec467d
raulhuamani <raul.huamani.h@gmail.com>
2024-02-20 22:36:08 -0500

    S20 - RestServer - Paginación - Middlewares - Category.dto y deshabilitar por parametro envio correo

---
+++ src/presentation/category/routes.ts
@@ -0,0 +1,19 @@
+import { envs } from "../../config";
+import { Router } from "express";
+import { CategoryService } from "../services/category.service";
+import { CategoryController } from "./controller";
+import { AuthMiddleware } from "../middlewares/auth.middleware";
+
+export class CategoryRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const categoryService = new CategoryService();
+    const controller = new CategoryController(categoryService);
+
+    // Definir las rutas
+    router.get("/", controller.getCategories);
+    router.post("/", [AuthMiddleware.validateJWT], controller.createCategory);
+
+    return router;
+  }
+}
